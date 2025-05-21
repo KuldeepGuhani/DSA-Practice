2dc4cd9328f57981a5121af909711f3df3c252dc
kd <kuldeep@asfera.in>
2025-05-21 15:44:22 +0530

    Added rotate array by 1 element Sucessfully

---
+++ Easy/rotateByOneArray.java
@@ -0,0 +1,22 @@
+
+public class rotateByOneArray {
+
+    public static void main(String[] args) {
+    //Left Rotate Array by 1 Element TC=0(N) and SC=0(1).
+    int []arr={1,2,3,4,5};
+        rotateByOne(arr);
+        for(int num:arr){
+            System.out.print(" "+num+"    ");
+        }
+    
+    }
+    
+    public static void rotateByOne(int[] arr) {
+        int temp=arr[0];
+     for(int i=0;i<arr.length-1;i++){    
+         arr[i]=arr[i+1];
+     }   
+     arr[arr.length-1]=temp;
+    }
+}
+
